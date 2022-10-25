c82e87f70b80fdbce2fd58e507f9258b033c3b90
Jovian Lim <kiriketsuki@gmail.com>
2022-10-25 20:59:03 +0800

    cleanup notebook, created class

--- classes/pose_detector.py
+++ classes/pose_detector.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import math
 import mediapipe as mp
 import pandas as pd
+from traitlets import default
 
 
-class process_image():
+class ImageProcessor():
     def __init__(self, image):
         self.sol = mp.solutions.pose
         self.mp_draw = mp.solutions.drawing_utils
@@ -22,8 +23,11 @@ class process_image():
                              'right_arm|right_torso', 'left_arm|left_torso',
                              'right_leg|right_torso', 'left_leg|left_torso']
         self.results = self.pose.process(self.original_image)
-        self.create_limbs()
         self.create_landmarks()
+        self.create_limbs()
+        self.create_angles()
+
+    # calculations 
 
     def create_landmarks(self):
         landmarks = {}
@@ -56,15 +60,26 @@ class process_image():
                 limbs[name] = {"vector": vector, "first_node": first_node, "second_node": second_node}
         self.limbs = limbs
 
-    def get_gradient(point_0, point_1):
+    def create_angles(self):
+        self.angles = {}
+        for angle in self.valid_angles:
+            limb_0, limb_1 = angle.split("|")
+            angle = self.calc_angle(self.limbs, limb_0, limb_1)
+            self.angles[f"{limb_0}|{limb_1}"] = angle
+
+    # helper functions
+
+    @staticmethod
+    def calc_gradient(point_0, point_1):
         return (point_1[1] - point_0[1]) / (point_1[0] - point_0[0])
 
-    def get_angle(self, limb_0, limb_1):
-        if limb_0 in self.limbs.keys() and limb_1 in self.limbs.keys():
-            limb_0_0 = self.limbs[limb_0]["first_node"]["image_coords"]
-            limb_0_1 = self.limbs[limb_0]["second_node"]["image_coords"]
-            limb_1_0 = self.limbs[limb_1]["first_node"]["image_coords"]
-            limb_1_1 = self.limbs[limb_1]["second_node"]["image_coords"]
+    @staticmethod
+    def calc_angle(limbs, limb_0, limb_1):
+        if limb_0 in limbs.keys() and limb_1 in limbs.keys():
+            limb_0_0 = limbs[limb_0]["first_node"]["image_coords"]
+            limb_0_1 = limbs[limb_0]["second_node"]["image_coords"]
+            limb_1_0 = limbs[limb_1]["first_node"]["image_coords"]
+            limb_1_1 = limbs[limb_1]["second_node"]["image_coords"]
 
             if limb_0_0 == limb_1_0:
                 a = limb_0_1
@@ -85,15 +100,30 @@ class process_image():
             else:
                 return None
 
-            m0 = self.get_gradient(a, common)
-            m1 = self.get_gradient(b, common)
+            m0 = ImageProcessor.calc_gradient(a, common)
+            m1 = ImageProcessor.calc_gradient(b, common)
             angle_rad = math.atan((m1 - m0) / (1 + m0 * m1))
             angle_deg = (math.degrees(angle_rad))
             angle_deg = angle_deg if angle_deg > 0 else 180 + angle_deg
-
             return angle_deg
         else:
             return None
+
+    # visualization
+    def draw_limbs(self, thickness=2, colour=(0,255,0)):
+        image = self.original_image.copy()
+        for limb in self.limbs:
+            first_node = self.limbs[limb]["first_node"]["image_coords"]
+            second_node = self.limbs[limb]["second_node"]["image_coords"]
+            cv.line(image, first_node, second_node, colour, thickness)
+        return image
+
+    def draw_landmarks(self, radius=3, colour=(0, 0, 255)):
+        image = self.original_image.copy()
+        for row in self.landmarks_df.iterrows():
+            landmark = row[1]
+            cv.circle(image, landmark["image_coords"], radius, colour, -1)
+        return image
 
     def label_limbs(self):
         image = self.original_image.copy()
@@ -124,11 +154,45 @@ class process_image():
                 common_node = limb_0_1
             else:
                 continue
-            angle = self.get_angle(limb_0, limb_1)
+            angle = self.calc_angle(self.limbs, limb_0, limb_1)
+            self.angles[f"{limb_0}|{limb_1}"] = angle
             x = common_node["image_coords"][0]
             y = common_node["image_coords"][1]
             cv.putText(image, str(round(angle, 1)), (int(x) + 20, int(y) + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv.LINE_AA)
         return image
+
+    # getters
+    def get_landmarks(self):
+        return self.landmarks_df
+    
+    def get_limbs(self):
+        return self.limbs
+
+    def get_angles(self):
+        return self.angles
+
+    def get_specific_angle(self, limb_0, limb_1):
+        if f"{limb_0}|{limb_1}" in self.angles.keys():
+            return self.angles[f"{limb_0}|{limb_1}"]
+        elif f"{limb_1}|{limb_0}" in self.angles.keys():
+            return self.angles[f"{limb_1}|{limb_0}"]
+        else:
+            raise Exception("Angle not found")
+
+    def display(self, option = 0):
+        match option:
+            case 0:
+                return self.original_image
+            case 1:
+                return self.label_limbs()
+            case 2:
+                return self.label_angles()
+            case 3:
+                return self.draw_landmarks()
+            case 4:
+                return self.draw_limbs()
+            case default:
+                return self.original_image
 
 def main():
     running_image = cv.imread("../inputs/run_0.jpg")
